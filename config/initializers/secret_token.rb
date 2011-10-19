f4dc58f4265c4ff538aae33a49b37f04a66b8ae2
Prescience-Toshiba <prescience-toshiba@prescience-toshiba.local.lan>
2011-10-19 09:25:02 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = 'd802b6fc023cf4abbb5b6b81536bd582d364299daa1b3d4d3ae2857367640524d61612e2195381e071baa8c11a95a20fc802874ed5697b6b78c3a9e178b643f6'
